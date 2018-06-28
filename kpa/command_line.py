40eebaf8a234e50da7899dab951b921a219bf2fa
Peter VandeHaar <pjvandehaar@gmail.com>
2018-06-28 19:05:20 -0400

    .

--- kpa/command_line.py
+++ kpa/command_line.py
@@ -7,5 +7,13 @@ def main():
         from .http_server import serve
         serve()
 
+    elif sys.argv[1:] == ['termcolor']:
+        from .terminal_utils import termcolor
+        def r(num): return '#' if num is None else str(num%10)
+        for fg in [None]+list(range(0,25)):
+            print(' '.join(termcolor(r(fg)+r(bg), fg, bg) for bg in [None]+list(range(50))))
+
+
     else:
+        # TODO: print all options
         print('unknown command:', sys.argv[1:])
