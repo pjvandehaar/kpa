a52b3571f10c8f84955a05c0909fdd4666e61f82
Peter VandeHaar <pjvandehaar@gmail.com>
2018-06-30 00:36:22 -0400

    .

--- kpa/command_line.py
+++ kpa/command_line.py
@@ -10,9 +10,10 @@ def main():
     elif sys.argv[1:] == ['termcolor']:
         from .terminal_utils import termcolor
         def r(num): return '#' if num is None else str(num%10)
+        print('    # ' + ' '.join(f'{bg:2}' for bg in range(50)))
         for fg in [None]+list(range(0,25)):
-            print(' '.join(termcolor(r(fg)+r(bg), fg, bg) for bg in [None]+list(range(50))))
-
+            print(f'{fg if fg is not None else "#":<2} ' +
+                  ' '.join(termcolor(r(fg)+r(bg), fg, bg) for bg in [None]+list(range(50))))
 
     else:
         # TODO: print all options
