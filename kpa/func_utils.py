5885b40773a29ea434fc8255b6fe3eda6069656f
Peter VandeHaar <pjvandehaar@gmail.com>
2020-12-12 23:49:49 -0800

    .

--- kpa/func_utils.py
+++ kpa/func_utils.py
@@ -1,41 +1,43 @@
 
-import boltons.funcutils
+import functools
+from typing import List, Callable, Any, Iterable, TypeVar, Tuple, Dict
 
-def assign(func): return func()
+T = TypeVar('T')
+T2 = TypeVar('T2')
+
+def assign(func: Callable[..., T]) -> T: return func()
 def assign_list(func): return list(func()) # TODO: prefer `@assign @list_from_iter`, which also allows cacheing
 def assign_dict(func): return dict(func())
 
-def only_once(func):
+def only_once(func: Callable[..., T]) -> Callable[..., T]:
     already_ran = [False] # TODO: use `nonlocal` or `global` or whatever to avoid `[0]`
-    @boltons.funcutils.wraps(func)
+    @functools.wraps(func)
     def f(*args, **kwargs):
         if not already_ran[0]:
             already_ran[0] = True
             return func(*args, **kwargs)
     return f
 
-# def list_from_iter(func):
-#     @boltons.funcutils.wraps(func)
-#     def f(*args, **kwargs):
-#         return list(func(*args, **kwargs))
-#     return f
+def list_from_iter(func: Callable[..., Iterable[T]]) -> Callable[..., List[T]]:
+    @functools.wraps(func)
+    def f(*args, **kwargs):
+        return list(func(*args, **kwargs))
+    return f
 
-# def dict_from_iter(func):
-#     @boltons.funcutils.wraps(func)
-#     def f(*args, **kwargs):
-#         return dict(func(*args, **kwargs))
-#     return f
+def dict_from_iter(func: Callable[..., Iterable[Tuple[T, T2]]]) -> Callable[..., Dict[T, T2]]:
+    @functools.wraps(func)
+    def f(*args, **kwargs):
+        return dict(func(*args, **kwargs))
+    return f
 
+# Too complex to typecheck
 def apply_to_result(*result_wrapper_funcs):
     def wrap(func):
-        @boltons.funcutils.wraps(func)
-        def f(*args, **kwargs):
+        @functools.wraps(func)
+        def f(*args, **kwargs) -> Any:
             result = func(*args, **kwargs)
             for result_wrapper_func in result_wrapper_funcs:
                 result = result_wrapper_func(result)
             return result
         return f
     return wrap
-
-list_from_iter = apply_to_result(list)
-dict_from_iter = apply_to_result(dict)
