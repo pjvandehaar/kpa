a722579f2e936f252511c399e99ee271b7aabf9a
Peter VandeHaar <pjvandehaar@gmail.com>
2024-01-18 17:55:02 -0500

    1.0.36

--- kpa/pip_utils.py
+++ kpa/pip_utils.py
@@ -1,8 +1,9 @@
 
 from kpa.func_utils import assign
-import re, json, urllib.request
+import re, json, urllib.request, functools
+import subprocess as subp
 from pathlib import Path
-from typing import List,Optional
+from typing import List,Optional,Dict
 
 
 def run(args:List[str]) -> None:
@@ -25,6 +26,7 @@ def run(args:List[str]) -> None:
 
 
 def check_file(filepath:str) -> None:
+    print('{:<2} {:20} {:11} {:11} {:11}'.format('', 'PACKAGE', 'SPEC', 'LATEST', 'INSTALLED'))
     with open(filepath) as f:
         for line in f:
             m = re.match(r'''^\s*'?([-a-zA-Z]+)(\[[a-zA-Z]+\])?([~<>=]{2}[0-9a-zA-Z\.]+)?'?,?\s*$''', line)
@@ -43,9 +45,34 @@ def check_pkg(pkg:str, opt:str, version:str, line:Optional[str] = None) -> None:
     if version is None: version=''
     try:
         j = json.loads(urllib.request.urlopen('https://pypi.org/pypi/{}/json'.format(pkg)).read())
-        latest = j['info']['version']
+        latest_version = j['info']['version']
         v = version.lstrip('~=>')
-        update_str = '' if latest == v or latest==v+'.0' or latest.startswith(v) else '>>'
-        print('{:<2} {:20} {:10} {:10}'.format(update_str, pkg+opt, version, latest))
+        installed_version = get_installed_pkg_versions2().get(pkg.lower(),'-')
+        update_str = (' ' if latest_version.startswith(v) else '>') + (' ' if installed_version.startswith(v) else '>')
+        print('{:<2} {:20} {:11} {:11} {:11}'.format(update_str, pkg+opt, version, latest_version, installed_version))
     except Exception:
         raise Exception([pkg, opt, version, line])
+
+@functools.cache
+def get_installed_pkg_versions() -> Dict[str,str]:
+    try: lines = subp.check_output(['pip3','freeze'], stderr=subp.DEVNULL).decode().split('\n')
+    except subp.CalledProcessError: return {}
+    ret = {}
+    for line in lines:
+        if '==' in line:
+            pkg, version = line.split('==', 1)
+            ret[pkg.lower()] = version
+    return ret
+
+@functools.cache
+def get_installed_pkg_versions2() -> Dict[str,str]:
+    try: lines = subp.check_output(['pip3','list'], stderr=subp.DEVNULL).decode().split('\n')
+    except subp.CalledProcessError: return {}
+    ret = {}
+    for idx, line in enumerate(lines):
+        if idx==0 and 'Package' in line or '-----' in line or not line.strip(): continue
+        parts  = line.split()
+        if len(parts) in (2,3):
+            pkg, version = parts[:2]
+            ret[pkg.lower()] = version if len(parts)==2 else f'{version}(editable)'
+    return ret
