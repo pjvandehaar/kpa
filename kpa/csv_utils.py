549c43cfd5d7b8f74843fa140e602e0a991f6782
Peter VandeHaar <pjvandehaar@gmail.com>
2024-05-16 08:04:42 -0400

    1.0.40

--- kpa/csv_utils.py
+++ kpa/csv_utils.py
@@ -4,24 +4,28 @@ import csv, io
 from typing import List,Dict,TextIO,Iterable
 
 
-def write_tsv(filename:str, data:Iterable[dict]) -> None:
+def write_tsv(filename:str, data:Iterable[dict], sep='\t') -> None:
     with open(filename, 'wt') as f:
-        write_tsv_to_fileobj(data, f)
+        write_tsv_to_fileobj(data, f, sep=sep)
 
-def as_tsv(data:Iterable[dict]) -> str:
+def as_tsv(data:Iterable[dict], sep='\t') -> str:
     stream = io.StringIO()
-    write_tsv_to_fileobj(data, stream)
+    write_tsv_to_fileobj(data, stream, sep=sep)
     return stream.getvalue()
 
-def write_tsv_to_fileobj(data:Iterable[dict], writer:TextIO) -> None:
+def write_tsv_to_fileobj(data:Iterable[dict], writer:TextIO, sep='\t') -> None:
     # TODO: Reduce RAM by gathering keys from first 10k rows, and hoping that remaining rows have same keys.
     data = [{str(key):val for key,val in row.items()} for row in data]
     colnames = []
     for row in data:
         for colname in row.keys():
             if colname not in colnames: colnames.append(colname)
-    writer.write('\t'.join(colnames) + '\n')
-    for row in data: writer.write('\t'.join(str(row.get(colname,'')) for colname in colnames) + '\n')
+    for idx,row in enumerate(data):
+        for val in row.values():
+            if sep in val:
+                raise Exception(f"Cannot write tsv because sep={repr(sep)} is in value {repr(val)} on line #{idx}")
+    writer.write(sep.join(colnames) + '\n')
+    for row in data: writer.write(sep.join(str(row.get(colname,'')) for colname in colnames) + '\n')
 
 
 def print_table(data:Iterable[dict], gutter:str='  ') -> None:
